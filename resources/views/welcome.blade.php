5f4a995506e639273b5c3799d07ede7bb84baf75
OscarDuranX <oscarduran@iesebre.com>
2015-12-08 23:31:35 +0100

    1a versio

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,15 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+    <link href="{{ asset('css/all.css')  }}" rel="stylesheet" type="text/css">
+    <link href="{{ asset('css/app.css')  }}" rel="stylesheet" type="text/css">
+</head>
+<body>
+<h1>Prova!</h1>
+
+<script src="https://cdnjs.cloudflare.com/ajax/libs/vue/1.0.10/vue.js"></script>
+<script src="{{ asset('js/all.js') }}"></script>
+</body>
+</html>
